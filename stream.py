5b475fa628e24cb45ee766ed5759ffcca92afaf9
Analyst-FPnA <analyst.fpna@gmail.com>
2024-10-02 14:32:58 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -88,7 +88,7 @@ def format_number(x):
 
 pivot1 = df_2205[df_2205['Master Kategori'].isin((df_2205['Master Kategori'].unique() if kategori=='ALL' else [kategori]))].groupby(['Nama Cabang','Month'])[['Kuantitas']].sum().reset_index().pivot(index='Nama Cabang',columns='Month',values='Kuantitas').reset_index()
 total = pd.DataFrame((pivot1.iloc[:,1:].sum(axis=0).values).reshape(1,len(pivot1.columns)-1),columns=pivot1.columns[1:])
-total['Nama Cabang']='TOTAL'+(pivot1['Nama Cabang'].str.len().max()+12)*' '
+total['Nama Cabang']='TOTAL'+(pivot1['Nama Cabang'].str.len().max()+20)*' '
 
 st.dataframe(pd.concat([pivot1,total])[:-1].style.format(lambda x: '' if x==0 else format_number(x)).background_gradient(cmap='Reds', axis=1, subset=pivot1.columns[1:]), use_container_width=True, hide_index=True)
 st.dataframe(pd.concat([pivot1,total])[-1:], use_container_width=True, hide_index=True)

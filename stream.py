d8ff18218cf4153a6004a7ab61f4737285091406
Analyst-FPnA <analyst.fpna@gmail.com>
2024-10-02 14:25:46 +0700

    Update stream.py

--- stream.py
+++ stream.py
@@ -103,4 +103,4 @@ st.dataframe(pivot1_can.style.format(lambda x: '' if x==0 else format_number(x))
 
 st.markdown('### ')
 pivot2_can = df_2205_can[(df_2205_can['Master Kategori'].isin((df_2205_can['Master Kategori'].unique() if kategori=='ALL' else [kategori]))) & (df_2205_can['Nama Cabang'].isin(df_2205_can['Nama Cabang'].unique() if cabang=='ALL' else [cabang]))].groupby(['Nama Barang','Month'])[['Kuantitas']].sum().reset_index().pivot(index='Nama Barang',columns='Month',values='Kuantitas').reset_index()
-st.dataframe(pivot2_can.style.format(lambd
+st.dataframe(pivot2_can.style.format(lambda x: '' if x==0 else format_number(x)).background_gradient(cmap='Reds', axis=1, subset=pivot2.columns[1:]), use_container_width=True, hide_index=True)
